5c2c054690b91b4f7b1a94cff33efa89956b872c
Nicolas Ayala <familiaayala061214@gmil.com>
2024-09-21 17:49:13 -0300

    The greeting was changed to: Task of Bons, Jame Bons.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,82 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] arg)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+
+        Console.Write("Please enter the percentage of your grade: ");
+        string percentaje = Console.ReadLine();
+        double per = double.Parse(percentaje);
+        string letter = "";
+        string sign = "";
+
+
+        
+
+
+        if ( per >= 97)
+        {
+            letter = "A";
+            sign = "+";
+            
+        }
+        else if ( per >= 94)
+        {
+            letter = "A";
+            
+        }
+        else if ( per >= 90)
+        {
+            letter = "A";
+            sign = "-";
+            
+        }
+        else if (per >= 87 )
+        {
+            letter = "B";
+            sign = "+";
+        }
+        else if (per >= 84 )
+        {
+            letter = "B";
+        }
+        else if (per >= 80 )
+        {
+            letter = "B";
+            sign = "-";
+        }
+        else if (per >= 77 )
+        {
+            letter = "C";
+            sign = "+";
+        }
+        else if (per >= 74 )
+        {
+            letter = "C";
+        }
+        else if (per >= 73 )
+        {
+            letter = "C";
+            sign = "-";
+        }
+        else if (per >= 67 )
+        {
+            letter = "D";
+            sign = "+";
+        }
+        else if (per >= 64 )
+        {
+            letter = "D";
+        }
+        else if (per >= 63 )
+        {
+            letter = "D";
+            sign = "-";
+        }
+        else
+        {
+            letter = "F";
+        }                        
+        Console.WriteLine($"{letter}{sign} {per}");
+    } 
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random ();
+        int number = randomGenerator.Next (1, 101);
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.Write("guess the number between 1 and 10: ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+            
+            if (guess > number)
+            {
+                Console.WriteLine(" Cool ! ");
+            }
+            else if  (guess < number)
+            {
+                Console.WriteLine("Hot !");
+            }
+            else 
+            {
+                Console.WriteLine($"You guessed it!, the number is {number}");
+            }
+        }
     }
 }
