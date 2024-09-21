b119372f205a3c5ea3cda0034bd803406f87c0ac
Nicolas Ayala <familiaayala061214@gmil.com>
2024-09-21 20:01:34 -0300

    The greeting from Prep4 was changed to the list task

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numb = new List<int>();
+
+        int number;
+
+        do 
+        {
+            Console.WriteLine("Enter the number or end with 0: ");
+            number = int.Parse (  Console.ReadLine() );
+
+            if (number != 0)
+            {
+                numb.Add(number);
+            }
+        } while (number != 0 );
+        Console.WriteLine("Number in the list: ");
+        foreach (int num in numb)
+        
+        {
+            Console.WriteLine(num);
+           
+        }
+        int sum = 0;
+
+        foreach (int num in numb)
+        {
+            sum += num;
+            
+        }
+        if (numb.Count > 0)
+        {
+            
+        
+        double average = (double) sum / numb.Count;
+        int max = numb.Max();
+        Console.WriteLine($"you have {numb.Count} numbers in your list");
+        Console.WriteLine ($"the sum of the all number is: {sum}");
+        Console.WriteLine ($"the average is {average}");
+        Console.WriteLine ($"the largestnumber is  {max}");
+        }
+    }   
 }
